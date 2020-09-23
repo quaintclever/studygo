698c0d3f44627a41cf1ffd235fe80ec1554347cf
quaint <qicong@ilarge.cn>
2020-09-23 11:22:51 +0800

    构建二叉树 代码提交

--- algorithm/algorithm_01.go
+++ algorithm/algorithm_01.go
@@ -7,9 +7,17 @@ import (
 
 func main() {
 
+	// 5. 构建二叉树
+	preorder := make([]int, 0)
+	preorder = append(preorder, 1, 2, 3)
+	inorder := make([]int, 0)
+	inorder = append(inorder, 2, 1, 3)
+	tree := buildTree(preorder, inorder)
+	fmt.Printf("%v", tree)
+
 	// 4. 写一个程序，统计一个字符串中每个单词出现的次数。
 	// 比如：”how do you do”中how=1 do=2 you=1。
-	printNum("how do you do")
+	//printNum("how do you do")
 
 	// 3. 求数组 2个数 和 为 8 的 坐标
 	//arrs := [5]int{1, 3, 5, 7, 8}
@@ -19,6 +27,41 @@ func main() {
 	//muTable()
 	// 1. 寻找出现一次的数字
 	//findOnceNum()
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 || len(inorder) == 0 {
+		return nil
+	}
+	tree := &TreeNode{
+		Val:   preorder[0],
+		Left:  nil,
+		Right: nil,
+	}
+	if len(preorder) == 1 {
+		return tree
+	}
+	var idx int
+	for i, v := range inorder {
+		if v == tree.Val {
+			idx = i
+		}
+	}
+	if idx == 0 {
+		tree.Right = buildTree(preorder[idx+1:], inorder[idx+1:])
+	} else if idx == len(inorder) {
+		tree.Left = buildTree(preorder[1:idx+1], inorder[:idx])
+	} else {
+		tree.Left = buildTree(preorder[1:idx+1], inorder[:idx])
+		tree.Right = buildTree(preorder[idx+1:], inorder[idx+1:])
+	}
+	return tree
 }
 
 // 4. 写一个程序，统计一个字符串中每个单词出现的次数。
